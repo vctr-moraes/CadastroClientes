945e6b526bb40dcd874b665800bbe1cf69a8630b
Victor Moraes <victor.moraes@outlook.com.br>
2025-08-13 19:17:03 -0300

    feat: Adiciona classes de modelo para Cliente, Endereco, Contato, Documento e TipoDocumento

---
+++ src/CadastroClientes.Business/Models/Contato.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CadastroClientes.Business.Models;
+
+[ExcludeFromCodeCoverage]
+public class Contato
+{
+    public string DescricaoContato { get; private set; }
+    public string NomeRepresentante { get; private set; }
+    public string EmailRepresentante { get; private set; }
+    public string TelefoneRepresentante { get; private set; }
+    public string TelefoneComercial { get; private set; }
+    public string EmailComercial { get; private set; }
+    public string Cargo { get; private set; }
+    public Cliente Cliente { get; private set; }
+    public Guid ClienteId { get; private set; }
+}
