633b0328cb44023425b6d2077946f27b9fa19023
Victor Moraes <victor.moraes@outlook.com.br>
2025-08-22 22:12:44 -0300

    feat: Adiciona viewmodels para Cliente, Contato, Documento e Endereco
          Foram criadas classes de modelo no namespace `CadastroClientes.App.Models` para representar as entidades `Cliente`, `Contato`, `Documento`, `Endereco` e `TipoDocumento`. Cada classe inclui propriedades com validações de dados, como chave primária, obrigatoriedade, comprimento máximo e formatação de exibição. Além disso, as classes possuem um atributo para excluir a cobertura de código nos testes. A classe `TipoDocumentoViewModel` foi implementada como um enum com três tipos de documentos: Contrato, Certidão e Certificado.

---
+++ src/CadastroClientes.App/Models/DocumentoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CadastroClientes.App.Models;
+
+[ExcludeFromCodeCoverage]
+public class DocumentoViewModel
+{
+    [Key] public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(500, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
+    [Display(Name = "Descrição")]
+    [DataType(DataType.Text)]
+    public string Descricao { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+    [Display(Name = "Data e Hora de Criação")]
+    [DataType(DataType.DateTime)]
+    public DateTime DataHoraCriacao { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    [Display(Name = "Tipo de Documento")]
+    public List<TipoDocumentoViewModel> TipoDocumentos { get; set; }
+
+    public List<SelectListItem> TipoDocumentosSelectListItem { get; set; }
+    public TipoDocumentoViewModel TipoDocumento { get; set; }
+
+    [ForeignKey("Cliente")] public Guid ClienteId { get; set; }
+}
